76875a131662db0ae487d58921fb341830402f72
Laiyingchuan <148051660+Laiyingchuan@users.noreply.github.com>
2023-10-18 11:14:27 +0800

    second commit

--- src/main/java/com/heiyun/git/GitTest.java
+++ src/main/java/com/heiyun/git/GitTest.java
@@ -2,6 +2,6 @@ package com.heiyun.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
+        System.out.println("hello git! 2.0版本");
     }
 }
