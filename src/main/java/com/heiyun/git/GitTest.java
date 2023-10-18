337d635fee44fc64087994c6922ff8a17c3bb583
Laiyingchuan <148051660+Laiyingchuan@users.noreply.github.com>
2023-10-18 15:16:42 +0800

    主线5.0

--- src/main/java/com/heiyun/git/GitTest.java
+++ src/main/java/com/heiyun/git/GitTest.java
@@ -4,5 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git! 主线4.0版本");
         System.out.println("hello git! 3.1支线版本");
+        System.out.println("hello git! 5.0主线版本");
+
     }
 }
